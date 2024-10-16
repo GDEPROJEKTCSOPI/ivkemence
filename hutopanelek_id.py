3d40ccbf71bc9d41efd29a1ef09f3e7970137880
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2024-10-16 16:32:46 +0200

    correction hutopanelek_id so it could be used anywhere

--- hutopanelek_id.py
+++ hutopanelek_id.py
@@ -1,53 +1,67 @@
 import pandas as pd
 
+# A hűtőpanelek CSV fájl betöltése
+file_path_hutopanelek = r'hutopanelek.csv'
+hutopanelek_df = pd.read_csv(file_path_hutopanelek, sep=';', encoding='utf-8')
+
 # Az adagok CSV fájl betöltése
-file_path_adagok = r'C:\Users\VG\Desktop\projektfeladat\adagok.csv'
-adagok_df = pd.read_csv(file_path_adagok, encoding='ISO-8859-1', sep=';')
+file_path_adagok = r'adagok.csv'
+adagok_df = pd.read_csv(file_path_adagok, encoding='utf-8', sep=';')
+
+# Convert 'Panel hőfok 1 [°C] Time' to datetime in hutopanelek_df
+hutopanelek_df['Panel hőfok 1 [°C] Time'] = pd.to_datetime(
+    hutopanelek_df['Panel hőfok 1 [°C] Time'], format='%Y.%m.%d %H:%M', errors='coerce'
+)
 
 # Új oszlop létrehozása - Kezdeti dátum és idő
-adagok_df['Kezdeti dátum és idő'] = pd.to_datetime(
-    adagok_df['Kezdeti dátum'] + ' ' + adagok_df['Kezdet idõ'], errors='coerce'
-)
-
 # Új oszlop létrehozása - Vége dátum és idő
-adagok_df['Vége dátum és idő'] = pd.to_datetime(
-    adagok_df['Vége dátum'] + ' ' + adagok_df['Vége idõ'], errors='coerce'
-)
+adagok_df['start_datetime'] = pd.to_datetime(adagok_df['Kezdeti datum'] + ' ' + adagok_df['Kezdet ido'], errors='coerce')
+adagok_df['end_datetime'] = pd.to_datetime(adagok_df['Vege datum'] + ' ' + adagok_df['Vege ido'], errors='coerce')
 
 # A DataFrame exportálása új CSV fájlba UTF-8 kódolással és pontosvesszővel elválasztva
-output_path_adagok = r'C:\Users\VG\Desktop\projektfeladat\adagok_atalakitott.csv'
+output_path_adagok = r'.\adagok_atalakitott.csv'
 adagok_df.to_csv(output_path_adagok, sep=';', encoding='utf-8', index=False)
 
 print(f"CSV fájl elmentve ide: {output_path_adagok}")
 
-# Hutopanelek csv betöltés
-file_path_hutopanelek = r'C:\Users\VG\Desktop\projektfeladat\Hutopanelek.csv'
-
-# csv-k betöltése pontosvesszővel elválasztva, utf-8 kódolással
-hutopanelek_df = pd.read_csv(file_path_hutopanelek, sep=';', encoding='utf-8')
+# Az átalakított file beolvasása
 adagok_atalakitott_df = pd.read_csv(output_path_adagok, sep=';', encoding='utf-8')
 
+#  Az idők átalakítása datetime formátumra az adagok fájlban
+adagok_atalakitott_df['start_datetime'] = pd.to_datetime(adagok_atalakitott_df['start_datetime'].str.strip().str.replace('\xa0', ''), errors='coerce')
+adagok_atalakitott_df['end_datetime'] = pd.to_datetime(adagok_atalakitott_df['end_datetime'].str.strip().str.replace('\xa0', ''), errors='coerce')
+
 # Az idők átalakítása datetime formátumra a hutopanelek fájlban
-hutopanelek_df['Panel hőfok 1 [°C] Time'] = pd.to_datetime(hutopanelek_df['Panel hőfok 1 [°C] Time'], errors='coerce', format='%Y.%m.%d %H:%M')
-
-# Az adagok H és I oszlopának (kezdeti és vége dátum/idő) átalakítása datetime formátumra
-start_times = pd.to_datetime(adagok_atalakitott_df['Kezdeti dátum és idő'], errors='coerce')
-end_times = pd.to_datetime(adagok_atalakitott_df['Vége dátum és idő'], errors='coerce')
-
-# Függvény az adagszám megkeresésére az idő alapján
-def find_adagszam_for_time(time, start_times, end_times, adagszam):
-    for start_time, end_time, adagszam_value in zip(start_times, end_times, adagszam):
-        if start_time <= time <= end_time:
-            return adagszam_value
-    return None
-
-# Az adagszám hozzárendelése közvetlenül az 'id' oszlopba (A oszlop)
-hutopanelek_df['id'] = hutopanelek_df['Panel hőfok 1 [°C] Time'].apply(
-    lambda x: find_adagszam_for_time(x, start_times, end_times, adagok_atalakitott_df['Adagszám'])
+hutopanelek_df['Panel hőfok 1 [°C] Time'] = pd.to_datetime(
+    hutopanelek_df['Panel hőfok 1 [°C] Time'], errors='coerce', infer_datetime_format=True
 )
 
-# Az eredmény exportálása UTF-8 kódolással, pontosvessző elválasztóval
-output_path_hutopanelek = r'C:\Users\VG\Desktop\projektfeladat\Hutopanelek_id_3.csv'
+# A nem idő vagyis type hibás adatokat töröljük
+hutopanelek_df.dropna(subset=['Panel hőfok 1 [°C] Time'], inplace=True)
+
+# IntervalIndex létrehozása
+intervals = pd.IntervalIndex.from_arrays(adagok_atalakitott_df['start_datetime'],
+                                         adagok_atalakitott_df['end_datetime'],
+                                         closed='both')
+
+# Függvény az adagszám megkeresésére az idő alapján
+def find_adagszam(time):
+    match = adagok_atalakitott_df.loc[intervals.contains(time), 'Adagszam']
+    if not match.empty:
+        return match.values[0]  # Return the first match
+    return None  # Return None if no match is found
+
+# Az adagszám hozzárendelése közvetlenül az 'id' oszlopba (A oszlop)
+hutopanelek_df['id'] = hutopanelek_df['Panel hőfok 1 [°C] Time'].apply(find_adagszam)
+
+# Id oszlop előre hozása és átnevezése
+cols = hutopanelek_df.columns.tolist()
+cols.insert(0, cols.pop(cols.index('id')))  # Move 'id' to the first position
+hutopanelek_df = hutopanelek_df[cols]
+hutopanelek_df.rename(columns={'id': 'Adagszam'}, inplace=True)
+
+# A DataFrame exportálása új CSV fájlba UTF-8 kódolással és pontosvesszővel elválasztva
+output_path_hutopanelek = r'hutopanelek_atalakitott.csv'
 hutopanelek_df.to_csv(output_path_hutopanelek, sep=';', encoding='utf-8', index=False)
 
-print(f"Az átalakított fájl mentve: {output_path_hutopanelek}")
+print(f"CSV fájl elmentve ide: {output_path_hutopanelek}")
