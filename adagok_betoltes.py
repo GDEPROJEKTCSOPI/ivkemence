c68461bf7f8cebbe3bfba0d64e5d4656e1c82ba6
VG <vgerg7@gmail.com>
2024-10-26 18:55:42 +0200

    2

--- adagok_betoltes.py
+++ adagok_betoltes.py
@@ -1,13 +1,12 @@
 import sqlite3
 import pandas as pd
 
-# 1. Adatbázis létrehozása és csatlakozás
-# Új adatbázis fájl létrehozása
+# Adatbázis létrehozása és csatlakozás
 path_to_db_df = r'C:\\sql\adagok_beemelese.db'
 conn = sqlite3.connect(r'C:\\sql\adagok_beemelese.db')
 cursor = conn.cursor()
 
-# 2. Táblák létrehozása az ER diagram alapján
+# Táblák létrehozása az ER diagram alapján
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS adagok (
     adag_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,7 +21,7 @@ cursor.execute('''
 cursor.execute('CREATE INDEX IF NOT EXISTS idx_adagok_start_datetime ON adagok(start_datetime);')
 cursor.execute('CREATE INDEX IF NOT EXISTS idx_adagok_adag_id ON adagok(adag_id);')
 
-# 3. Adatok betöltése az 'adagok_atalakitott.csv' fájlból
+# Adatok betöltése az 'adagok_atalakitott.csv' fájlból
 # CSV betöltése és oszlopok átnevezése a tábla struktúrájának megfelelően
 adagok_df = pd.read_csv(r'C:\Users\VG\Desktop\adatbeemeles/adagok_atalakitott.csv', delimiter=';')
 adagok_df = adagok_df.rename(columns={
@@ -33,8 +32,8 @@ adagok_df = adagok_df.rename(columns={
 })
 adagok_data = adagok_df[['start_datetime', 'end_datetime', 'koztes_ido', 'idotartam']]
 
-# 4. Adatok beillesztése az 'adagok' táblába (adag_id nélkül, mivel az automatikus)
+# Adatok beillesztése az 'adagok' táblába (adag_id nélkül, mivel az automatikus)
 adagok_data.to_sql('adagok', conn, if_exists='append', index=False)
 
-# 5. Kapcsolat lezárása
+# Kapcsolat lezárása
 conn.close()
