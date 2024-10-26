dcf8d4a853a88e3e6817b7f46d270714fea9ba9a
VG <vgerg7@gmail.com>
2024-10-26 18:54:53 +0200

    2

--- hutopanelek_betoltes.py
+++ hutopanelek_betoltes.py
@@ -1,12 +1,12 @@
 import sqlite3
 import pandas as pd
 
-# 1. Csatlakozás a meglévő adatbázishoz
+# Csatlakozás a meglévő adatbázishoz
 path_to_db_df = r'C:\\sql\hutopanelek_betoltes.db'
 conn = sqlite3.connect(r'C:\\sql\hutopanelek_betoltes.db')
 cursor = conn.cursor()
 
-# 2. 'hutopanelek' tábla létrehozása, ha még nem létezik
+# 'hutopanelek' tábla létrehozása, ha még nem létezik
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS hutopanelek (
     hutopanel_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,7 +14,7 @@ cursor.execute('''
     )
 ''')
 
-# 3. Adatok létrehozása és betöltése a 'hutopanelek' táblába
+# Adatok létrehozása és betöltése a 'hutopanelek' táblába
 # Panel neveinek generálása "Panel 1" - "Panel 15" formátumban
 panel_data = pd.DataFrame({
     'name': [f'Hűtőpanel {i}' for i in range(1, 16)]
@@ -23,5 +23,5 @@ panel_data = pd.DataFrame({
 # Adatok beillesztése a 'hutopanelek' táblába
 panel_data.to_sql('hutopanelek', conn, if_exists='append', index=False)
 
-# 4. Kapcsolat lezárása
+# Kapcsolat lezárása
 conn.close()
