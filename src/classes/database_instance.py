2ce6c30832c0c190d6ac4eb80de035560b924b01
Gáhor Krisztián <krisztian@gahor.hu>
2024-11-01 23:44:11 +0100

    adatbázis path módosítás, main takarítás

--- src/classes/database_instance.py
+++ src/classes/database_instance.py
@@ -1,5 +1,5 @@
 from src.classes.database import Database
 
-DB_PATH = 'database/ivkemence.db'
+DB_PATH = '../db_creator/output/ivkemence.db'
 
 db = Database(database_file=DB_PATH)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,4 @@
-from src.classes.database_instance import db
-from src.classes.database_manipulation import *
 from src.classes.menu import Menu
 
 # Főprogram indítás
-
 Menu().cmdloop()
-
-#insert_example()
-
-#df = db.query("SELECT * FROM homerseklet WHERE datumido LIKE date() || '%';")
-
-#print(df)
-
-#db.close()
